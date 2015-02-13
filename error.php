ad3427ad56c9c97762bd348089150b1c0e917f2e
Phoenix Eve Aspacio <aspaciop@gmail.com>
2015-02-14 07:02:35 +0800

    Peca Theme Files

---
+++ error.php
@@ -0,0 +1,35 @@
+<?php
+$delay = '5';
+error_reporting(0);
+$status=$_SERVER['REDIRECT_STATUS'];
+$codes=array(
+      400 => array('Bad Request', 'Your browser sent a request that this server could not understand. You will be redirected to homepage in '.$delay.' seconds.'),
+      401 => array('Authorization Required', 'This server could not verify that you are authorized to access the directory or document you requested. You will be redirected to homepage in '.$delay.' seconds.'),
+      403 => array('Forbidden', 'You don\'t have enough permissions to access the directory or document you requested. You will be redirected to homepage in '.$delay.' seconds.'),
+      404 => array('Not Found', 'The directory of document you requested was not found. You will be redirected to homepage in '.$delay.' seconds.'),
+      500 => array('Internal Server Error', 'The server encountered an internal error or misconfiguration and was unable to complete your request. You will be redirected to homepage in '.$delay.' seconds.'),
+);
+
+$errortitle=$codes[$status][0];
+$message=$codes[$status][1];
+
+if($errortitle==false){
+       $errortitle = 'Unknown Error';
+       $message = 'An unknown error has occurred. You will be redirected to homepage in '.$delay.' seconds.';
+}
+?>
+<html>
+<head>
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Courgette">
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Coming+Soon">
+<link rel="stylesheet" href="assets/css/main.css">
+<meta http-equiv="refresh" content="<?php echo $delay;?>;url=\">
+<title><?php echo $errortitle;?></title>
+</head>
+<body>
+<?php
+echo '<h1>'.$errortitle.'</h1>';
+echo '<center><p>'.$message.'</p></center>';
+?>
+</body>
+</html>
---
+++ opensearch.php
@@ -0,0 +1,38 @@
+<?php
+// format as xml
+header('Content-Type: text/xml');
+
+// load kirby
+define('DS', DIRECTORY_SEPARATOR);
+require(__DIR__ . DS . 'kirby' . DS . 'bootstrap.php');
+$kirby = kirby();
+$site = $kirby->site();
+
+// url
+function siteURL()
+{
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    $domainName = $_SERVER['HTTP_HOST'].'/';
+    return $protocol.$domainName;
+}
+?>
+<?xml version="1.0" encoding="UTF-8"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+    <ShortName><?php echo $site->title() ?></ShortName>
+    <Description><?php echo $site->description() ?></Description>
+    <Tags><?php echo $site->keywords() ?></Tags>
+    <Contact><?php echo $site->email() ?></Contact>
+    <Url type="text/html" template="<?php echo siteurl() ?>?q={searchTerms}"/>
+    <Url type="application/opensearchdescription+xml" rel="self" template="<?php echo siteurl().'opensearch.php' ?>"/>
+    <LongName><?php echo $site->title().' Search' ?></LongName>
+    <Image height="64" width="64" type="image/png"><?php echo siteurl().'assets/images/logo.png' ?></Image>
+    <Image height="16" width="16" type="image/x-icon"><?php echo siteurl().'favicon.ico' ?></Image>
+    <Query role="example" searchTerms="outdoor" />
+    <Developer><?php echo $site->title().' Development Team' ?></Developer>
+    <Attribution><?php echo 'Copyright '.date('Y').' '.$site->title() ?></Attribution>
+    <SyndicationRight>open</SyndicationRight>
+    <AdultContent>false</AdultContent>
+    <Language>en-us</Language>
+    <OutputEncoding>UTF-8</OutputEncoding>
+    <InputEncoding>UTF-8</InputEncoding>
+</OpenSearchDescription>
