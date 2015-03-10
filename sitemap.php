5d555aa686ca1c3706c7d9b5676631b8eabbc555
Phoenix Eve Aspacio <aspaciop@gmail.com>
2015-03-10 19:59:18 +0800

    Included a a new line

--- sitemap.php
+++ sitemap.php
@@ -24,6 +24,7 @@ echo '<?xml version="1.0" encoding="utf-8"?>';
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <?php foreach($pages->visible()->index() as $p): ?>
+<?php if(in_array($p->uri(), $ignore)) continue ?>
 
 	<url>
 <?php if(c::get('url')): ?>
