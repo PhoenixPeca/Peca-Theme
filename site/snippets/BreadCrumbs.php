1800aa48c32dd2e75fe22bf034ebfbf3eaff251e
Phoenix Eve Aspacio <aspaciop@gmail.com>
2015-12-15 13:30:00 +0800

    XSS vulnerability fixed

--- site/snippets/BreadCrumbs.php
+++ site/snippets/BreadCrumbs.php
@@ -3,7 +3,7 @@
 <?php if($peca->breadcrumb()->count() > 0): ?>
 <ol class="breadcrumb">
 <?php foreach($peca->breadcrumb() as $crumb): ?>
-<li<?php if($crumb->isActive() == true): ?> class="active"<?php endif ?>><?php if($crumb->isActive() == false): ?><a href="<?php echo $crumb->url() ?>"><?php endif ?><?php echo html($crumb->title()) ?><?php if($crumb->isActive() == false): ?></a><?php endif ?></li>
+<li<?php if($crumb->isActive() == true): ?> class="active"<?php endif ?>><?php if($crumb->isActive() == false): ?><a href="<?php echo $crumb->url() ?>"><?php endif ?><?php echo htmlspecialchars(html($crumb->title()), ENT_QUOTES, 'UTF-8') ?><?php if($crumb->isActive() == false): ?></a><?php endif ?></li>
 <?php endforeach ?>
 </ol>
 <?php endif ?>
